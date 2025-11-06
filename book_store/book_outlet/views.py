61a37e900b3365c9ac0f04f75cda71de6b22178b
vungocdat <ngoc.dat.vu96@gmail.com>
2025-11-06 22:56:01 +0100

    book store - added more functions: order, average, number of books

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Avg
 
 from .models import Book
 
@@ -6,9 +7,13 @@ from .models import Book
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('-rating')
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))
     return render(request, 'book_outlet/index.html', {
-        'books': books
+        'books': books,
+        'num_books': num_books,
+        'avg_rating': avg_rating
     })
 
 
