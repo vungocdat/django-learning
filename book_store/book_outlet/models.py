aa838abd35b30a61f455c0966407383660b664d8
vungocdat <ngoc.dat.vu96@gmail.com>
2025-11-07 10:20:49 +0100

    book store - admin site, connecting admin with db

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -11,7 +11,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])  # only values from 1 to 5
     author = models.CharField(null=True, max_length=100)
     bestseller = models.BooleanField(default=False)
-    slug = models.SlugField(default='', null=False, db_index=True)  # Harry Potter 1 -> harry-potter-1, will be created during the save()
+    slug = models.SlugField(default='', blank=True, editable=False, null=False, db_index=True)  # Harry Potter 1 -> harry-potter-1, will be created during the save()
 
     def __str__(self):
         return f'{self.title} ({self.rating})'
