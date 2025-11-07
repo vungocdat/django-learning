d4ed60a02d42ad627199e211601b85064a3c6b47
vungocdat <ngoc.dat.vu96@gmail.com>
2025-11-07 10:29:30 +0100

    book store - slug field prepopulated

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -4,6 +4,9 @@ from .models import Book
 
 # Register your models here.
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
 
 
+admin.site.register(Book, BookAdmin)
